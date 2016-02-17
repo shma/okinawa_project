42ba4a82bda24115ae4b967caef783aff41f4c69
Shunya Matsuno <id.shma@gmail.com>
2016-02-17 23:43:52 +0900

    沖縄に寄るように調整

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,10 @@
 (function() {
   "use strict";
   var viewer = new Cesium.Viewer("cesium");
+  var scene = viewer.scene;
+
+  viewer.camera.flyTo({
+        destination : Cesium.Cartesian3.fromDegrees(127.8040432, 26.3637929, 15000.0)
+  });
+
 }());
